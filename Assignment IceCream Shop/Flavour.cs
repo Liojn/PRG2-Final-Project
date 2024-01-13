5ae931ced82b606db96e945da346fba08da74bd8
3Kr15 <150001216+3Kr15@users.noreply.github.com>
2024-01-13 16:52:11 +0800

    Add files via upload

---
+++ Assignment IceCream Shop/Flavour.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_IceCream_Shop
+{
+    class Flavour 
+    {
+		private string type;
+
+		public string Type
+		{
+			get { return type; }
+			set { type = value; }
+		}
+
+		private bool premium;
+
+		public bool Premium
+		{
+			get { return premium; }
+			set { premium = value; }
+		}
+
+		private int quantity;
+
+		public int Quantity
+		{
+			get { return quantity; }
+			set { quantity = value; }
+		}
+
+		public Flavour() { }
+		public Flavour(string t, bool p, int q)
+		{
+			Type = t;
+			Premium = p;
+			Quantity = q;
+		}
+
+		public override string ToString()
+		{
+			return "Type: " + Type + "Premium: " + Premium + "Quantity: " + Quantity;
+		}
+
+
+	}
+}
